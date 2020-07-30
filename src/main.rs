0bf49fd2f829e124a9dd82271e399a42743cf030
Mikastiv <m.leblanc_3@hotmail.com>
2020-07-30 12:55:24 -0400

    background color + spread extra pixels evenly

--- src/main.rs
+++ src/main.rs
@@ -51,11 +51,24 @@ fn hit(ray: Ray) -> bool {
     discriminant > 0.0
 }
 
-fn get_color(ray: Ray) -> RGBColor {
+#[allow(clippy::many_single_char_names)]
+fn get_background_color(x: usize, y: usize) -> RGBColor {
+    let r = x as f64 / (IMG_WIDTH - 1) as f64;
+    let g = y as f64 / (IMG_HEIGHT - 1) as f64;
+    let b = 0.25;
+
+    RGBColor {
+        r: (r * 255.99) as u8,
+        g: (g * 255.999) as u8,
+        b: (b * 255.999) as u8,
+    }
+}
+
+fn get_color(x: usize, y: usize, ray: Ray) -> RGBColor {
     if hit(ray) {
         RGBColor { r: 255, g: 0, b: 0 }
     } else {
-        RGBColor { r: 0, g: 255, b: 0 }
+        get_background_color(x, y)
     }
 }
 
@@ -66,10 +79,7 @@ fn render_surface(
     height: usize,
     cam: Camera,
     progress_bar: ProgressBar,
-    thread_no: usize,
 ) -> Surface {
-    progress_bar.set_message(&format!("Thread {}", thread_no));
-
     let mut surface = Surface::new(x_offset, y_offset, width, height);
     for j in 0..height {
         progress_bar.inc(1);
@@ -77,11 +87,11 @@ fn render_surface(
             let u = (i + x_offset) as f64 / (IMG_WIDTH - 1) as f64;
             let v = (j + y_offset) as f64 / (IMG_HEIGHT - 1) as f64;
             let ray = cam.get_ray(u, v);
-            let color = get_color(ray);
+            let color = get_color(i + x_offset, j + y_offset, ray);
             surface.set_color(i, j, color);
         }
     }
-    progress_bar.finish_with_message("done");
+    progress_bar.finish_with_message("Done");
 
     surface
 }
@@ -97,6 +107,7 @@ fn main() {
         DIST_TO_FOCUS,
     );
 
+    // If I use more than 12 threads, the program crashed on my PC
     let available_threads = num_cpus::get();
     let thread_count = if available_threads > 12 {
         12
@@ -105,41 +116,51 @@ fn main() {
     };
 
     let section_height = IMG_HEIGHT / thread_count;
-    let last_section_extra_pixels = IMG_HEIGHT % thread_count;
+    let mut extra_pixels = IMG_HEIGHT % thread_count;
     let (tx, rx) = channel();
 
     println!("Using {} threads", thread_count);
+
+    // Progress bar setup
     let multi_progress = MultiProgress::new();
     let progress_style = ProgressStyle::default_bar()
-        .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}")
-        .progress_chars("##-");
+        .template("[{elapsed_precise}] {prefix:>10}: {bar:40.cyan/blue} {pos:>5}/{len:5} {msg}")
+        .progress_chars("=>-");
 
+    let mut height_offset = 0;
     for i in 0..thread_count {
         let local_camera = camera.clone();
 
         let child_tx = tx.clone();
-        let surface_height = if i == thread_count - 1 {
-            section_height + last_section_extra_pixels
+
+        // Spread extra pixels evenly across threads
+        let surface_height = if extra_pixels > 0 {
+            extra_pixels -= 1;
+            section_height + 1
         } else {
             section_height
         };
 
         let progress_bar = multi_progress.add(ProgressBar::new(surface_height as u64));
         progress_bar.set_style(progress_style.clone());
+        progress_bar.set_message("Scanlines remaining");
+        let prefix = format!("Thread {}", i);
+        progress_bar.set_prefix(prefix.as_str());
 
         thread::spawn(move || {
             child_tx
                 .send(render_surface(
                     0,
-                    i * section_height,
+                    height_offset,
                     IMG_WIDTH,
                     surface_height,
                     local_camera,
                     progress_bar,
-                    i,
                 ))
                 .unwrap();
         });
+
+        height_offset += surface_height;
     }
     multi_progress.join().unwrap();
 
